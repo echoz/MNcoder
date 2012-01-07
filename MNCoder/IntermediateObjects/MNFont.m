518543145911870a76fc52d2e3c553acbc52a403
Jeremy Foo <jeremyfoo@gmail.com>
2012-01-07 09:46:19 +0800

    added intermediate objects for color and font

---
+++ MNCoder/IntermediateObjects/MNFont.m
@@ -0,0 +1,13 @@
+//
+//  MNFont.m
+//  Mac
+//
+//  Created by Jeremy Foo on 1/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MNFont.h"
+
+@implementation MNFont
+
+@end
