64a236de8fff2b2ee686f527b1989b558c4e2d76
Jeremy Foo <jeremyfoo@gmail.com>
2012-01-08 00:45:53 +0800

    MNColor that assumes a calibrated color space for NSColor for conversion to and from UIColor.

--- MNCoder/IntermediateObjects/MNColor.h
+++ MNCoder/IntermediateObjects/MNColor.h
@@ -6,8 +6,27 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#else
 #import <Foundation/Foundation.h>
+#endif
 
-@interface MNColor : NSObject <NSCoding>
+#import "MNCIntermediateObjectProtocol.h"
+
+@interface MNColor : NSObject <NSCoding, MNCIntermediateObjectProtocol>
+
+@property (readwrite) CGFloat red;
+@property (readwrite) CGFloat green;
+@property (readwrite) CGFloat blue;
+@property (readwrite) CGFloat alpha;
+
+#if TARGET_OS_IPHONE
+-(id)initWithColor:(UIColor *)color;
+-(UIColor *)color;
+#else 
+-(id)initWithColor:(NSColor *)color;
+-(NSColor *)color;
+#endif
 
 @end
--- MNCoder/IntermediateObjects/MNColor.m
+++ MNCoder/IntermediateObjects/MNColor.m
@@ -9,5 +9,63 @@
 #import "MNColor.h"
 
 @implementation MNColor
+@synthesize red = _red, green = _green, blue = _blue, alpha = _alpha;
+
+#pragma mark - NSCoding Protocol
+
+-(id)initWithCoder:(NSCoder *)aDecoder {
+	if ((self = [super init])) {
+		self.red = [aDecoder decodeFloatForKey:@"red"];
+		self.green = [aDecoder decodeFloatForKey:@"green"];
+		self.blue = [aDecoder decodeFloatForKey:@"blue"];
+		self.alpha = [aDecoder decodeFloatForKey:@"alpha"];
+	}
+	
+	return self;
+}
+
+-(void)encodeWithCoder:(NSCoder *)aCoder {
+	[aCoder encodeFloat:self.red forKey:@"red"];
+	[aCoder encodeFloat:self.green forKey:@"green"];
+	[aCoder encodeFloat:self.blue forKey:@"blue"];
+	[aCoder encodeFloat:self.alpha forKey:@"alpha"];
+}
+
+#pragma mark - Platform specific representation
+
+#if TARGET_OS_IPHONE
+
+-(id)initWithColor:(UIColor *)color {
+	if ((self = [super init])) {
+		[color getRed:&_red green:&_green blue:&_blue alpha:&_alpha];
+	}
+	return self;
+}
+
+-(UIColor *)color {
+	return [UIColor colorWithRed:self.red green:self.green blue:self.blue alpha:self.alpha];
+}
+
+#else
+
+-(id)initWithColor:(NSColor *)color {
+	if ((self = [super init])) {
+		// need to convert to a calibrated color space first
+		NSColor *calibratedColor = [color colorUsingColorSpace:[NSColorSpace genericRGBColorSpace]];
+		[calibratedColor getRed:&_red green:&_green blue:&_blue alpha:&_alpha];
+	}
+	return self;
+}
+
+-(NSColor *)color {
+	return [NSColor colorWithCalibratedRed:self.red green:self.green blue:self.blue alpha:self.alpha];
+}
+#endif
+
+#pragma mark - MNCIntermediateObject Protocol
+
+-(id)platformRepresetnation {
+	return [self color];
+}
 
 @end
