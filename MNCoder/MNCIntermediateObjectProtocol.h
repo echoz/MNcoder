90b5faa01df3ea6866330c6477e7d6e193f4bcb4
Jeremy Foo <jeremyfoo@gmail.com>
2012-01-07 15:23:16 +0800

    Added Intermediate Object Protocol

---
+++ MNCoder/MNCIntermediateObjectProtocol.h
@@ -0,0 +1,15 @@
+//
+//  MNKeyedArchiver.h
+//  Mac
+//
+//  Created by Jeremy Foo on 1/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MNCIntermediateObjectProtocol <NSObject>
+
+<#methods#>
+
+@end
