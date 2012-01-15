3ade4787c03b62282b47c8792d4d989137584619
Jeremy Foo <jeremyfoo@gmail.com>
2012-01-16 03:38:42 +0800

    mnglyph able to translate and store intermediate state

--- MNCoder/IntermediateObjects/NSAttributedStrings/MNGlyphInfo.h
+++ MNCoder/IntermediateObjects/NSAttributedStrings/MNGlyphInfo.h
@@ -7,11 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#if TARGET_OS_IPHONE
+#import <CoreText/CoreText.h>
+#endif
 
 @interface MNGlyphInfo : NSObject <NSCoding>
 
 @property (readonly) NSUInteger characterCollection;
 @property (readonly) NSUInteger characterIdentifier;
 @property (readonly) NSString *baseString;
+
+-(id)initWithGlyphRef:(CTGlyphInfoRef)glyphref baseString:(NSString *)baseString;
+
+#if TARGET_OS_IPHONE
+-(CTGlyphInfoRef)platformRepresentation;
+#else
+-(NSGlyphInfo *)platformRepresentation;
+#endif
 
 @end
