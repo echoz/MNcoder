0ceec4bb8d0830646ee9ef611ec1bd0067b75ae8
Jeremy Foo <jeremyfoo@gmail.com>
2012-01-17 03:55:41 +0800

    properly added stuff to encode

--- MNCoder/IntermediateObjects/NSAttributedStrings/MNParagraphyStyle.m
+++ MNCoder/IntermediateObjects/NSAttributedStrings/MNParagraphyStyle.m
@@ -19,12 +19,42 @@
 
 -(id)initWithCoder:(NSCoder *)aDecoder {
 	if ((self = [super init])) {
+		_tabStops = [[aDecoder decodeObjectForKey:@"tabStops"] copy];
+		_alignment = [[aDecoder decodeObjectForKey:@"alignment"] unsignedIntegerValue];
+		_lineBreakMode = [[aDecoder decodeObjectForKey:@"lineBreakMode"] unsignedIntegerValue];
+		_baseWritingDirection = [[aDecoder decodeObjectForKey:@"baseWritingDirection"] unsignedIntegerValue];
 		
+		_firstLineHeadIndent = [aDecoder decodeFloatForKey:@"firstLineHeadIdent"];
+		_headIndent = [aDecoder decodeFloatForKey:@"headIndent"];
+		_tailIndent = [aDecoder decodeFloatForKey:@"tailIndent"];
+		_defaultTabInterval = [aDecoder decodeFloatForKey:@"defaultTabInterval"];
+		_lineHeightMultiple = [aDecoder decodeFloatForKey:@"lineHeightMultiple"];
+		_maximumLineHeight = [aDecoder decodeFloatForKey:@"maximumLineHeight"];
+		_minimumLineHeight = [aDecoder decodeFloatForKey:@"minimumLineHeight"];
+		_lineSpacing = [aDecoder decodeFloatForKey:@"lineSpacing"];
+		_paragraphSpacing = [aDecoder decodeFloatForKey:@"paragraphSpacing"];
+		_paragraphSpacingBefore = [aDecoder decodeFloatForKey:@"paragraphSpacingBefore"];
 	}
 	return self;
 }
 
 -(void)encodeWithCoder:(NSCoder *)aCoder {
+	[aCoder encodeObject:self.tabStops forKey:@"tabStops"];
+	[aCoder encodeObject:[NSNumber numberWithUnsignedInteger:self.alignment] forKey:@"alignment"];
+	
+	[aCoder encodeObject:[NSNumber numberWithUnsignedInteger:self.lineBreakMode] forKey:@"lineBreakMode"];
+	[aCoder encodeObject:[NSNumber numberWithUnsignedInteger:self.baseWritingDirection] forKey:@"baseWritingDirection"];
+	
+	[aCoder encodeFloat:self.firstLineHeadIndent forKey:@"firstLineHeadIdent"];
+	[aCoder encodeFloat:self.headIndent forKey:@"headIndent"];
+	[aCoder encodeFloat:self.tailIndent forKey:@"tailIndent"];
+	[aCoder encodeFloat:self.defaultTabInterval forKey:@"defaultTabInterval"];
+	[aCoder encodeFloat:self.lineHeightMultiple forKey:@"lineHeightMultiple"];
+	[aCoder encodeFloat:self.maximumLineHeight forKey:@"maximumLineHeight"];
+	[aCoder encodeFloat:self.minimumLineHeight forKey:@"minimumLineHeight"];
+	[aCoder encodeFloat:self.lineSpacing forKey:@"lineSpacing"];
+	[aCoder encodeFloat:self.paragraphSpacing forKey:@"paragraphSpacing"];
+	[aCoder encodeFloat:self.paragraphSpacingBefore forKey:@"paragraphSpacingBefore"];
 	
 }
 
