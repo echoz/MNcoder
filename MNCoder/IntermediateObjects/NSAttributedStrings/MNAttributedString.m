0a0474084f4579490a672cd9e9b7335720e36773
Jeremy Foo <jeremyfoo@gmail.com>
2012-01-19 03:15:09 +0800

    building of intermediate attributes for Mac working

--- MNCoder/IntermediateObjects/NSAttributedStrings/MNAttributedString.m
+++ MNCoder/IntermediateObjects/NSAttributedStrings/MNAttributedString.m
@@ -7,10 +7,11 @@
 //
 
 #import "MNAttributedString.h"
+#import "MNParagraphyStyle.h"
 
 @interface MNAttributedString (/* Private Methods */)
 -(void)_buildIntermediateRepresentationFromString:(NSAttributedString *)string;
--(NSDictionary *)dictionaryForAttribute:(NSString *)attributeName value:(id<NSCoding>)value range:(NSRange)aRange;
+-(NSDictionary *)_dictionaryForAttributes:(NSDictionary *)attrs range:(NSRange)aRange;
 
 @end
 
@@ -22,7 +23,7 @@
 -(id)initWithCoder:(NSCoder *)aDecoder {
 	if ((self = [super init])) {
 		_string = [[aDecoder decodeObjectForKey:@"string"] copy];
-		_attributes = [[aDecoder decodeObjectForKey:@"attributes"] retain];
+		_attributes = [[aDecoder decodeObjectForKey:@"attributes"] copy];
 	}
 	return self;
 }
@@ -56,20 +57,45 @@
 
 -(void)_buildIntermediateRepresentationFromString:(NSAttributedString *)string {
 	_string = [string.string copy];
-	NSMutableArray *attrs = [NSMutableArray arrayWithCapacity:0];
-	
+	NSMutableArray *attributes = [NSMutableArray arrayWithCapacity:0];
+
+#if TARGET_OS_IPHONE
 	[string enumerateAttributesInRange:NSMakeRange(0, [_string length]) options:NSAttributedStringEnumerationReverse usingBlock:^(NSDictionary *attrs, NSRange range, BOOL *stop) {
 		
+		
+		
+		[attributes insertObject:[self _dictionaryForAttributes:attrs range:range] atIndex:([attributes count]-1)];
+	}];
+#else
+	[string enumerateAttributesInRange:NSMakeRange(0, [_string length]) options:NSAttributedStringEnumerationReverse usingBlock:^(NSDictionary *attrs, NSRange range, BOOL *stop) {
+		NSMutableDictionary *finalAttributes = [NSMutableDictionary dictionaryWithCapacity:[attrs count]];
+		
+		for (NSString *key in attrs) {
+			if ([[attrs objectForKey:key] isKindOfClass:[NSParagraphStyle class]]) {
+				[finalAttributes setObject:[MNParagraphyStyle paragraphStyleWithStyle:[attrs objectForKey:key]] forKey:@"MNParagraphStyle"];
+			} else {
+				[finalAttributes setObject:[attrs objectForKey:key] forKey:key];
+			}
+		}
+		
+		[attributes insertObject:[self _dictionaryForAttributes:finalAttributes range:range] atIndex:(([attributes count] > 0)?([attributes count] - 1):0)];
 	}];
 	
-
+#endif
+	
+	_attributes = [attributes copy];
 }
 
--(NSDictionary *)dictionaryForAttribute:(NSString *)attributeName value:(id<NSCoding>)value range:(NSRange)aRange {
+-(void)dealloc {
+	[_string release], _string = nil;
+	[_attributes release], _string = nil;
+	[super dealloc];
+}
+
+-(NSDictionary *)_dictionaryForAttributes:(NSDictionary *)attrs range:(NSRange)aRange {
 	NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:3];
-	[dict setObject:attributeName forKey:@"attributeName"];
 	[dict setObject:[NSValue valueWithRange:aRange] forKey:@"range"];
-	[dict setObject:value forKey:@"value"];
+	[dict setObject:attrs forKey:@"attrs"];
 	
 	return dict;
 }
