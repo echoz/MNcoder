f74ab8ef84ada38fb77b65ed334d3260ea1b7869
Jeremy Foo <jeremyfoo@gmail.com>
2012-02-21 22:53:32 +0800

    additional ifdefs

--- MNCoder/MNUnarchiver.m
+++ MNCoder/MNUnarchiver.m
@@ -31,6 +31,14 @@
 #define __has_feature(x) 0 // Compatibility with non-clang compilers.
 #endif
 
+#ifndef NS_RETURNS_RETAINED
+#if __has_feature(attribute_ns_returns_retained)
+#define NS_RETURNS_RETAINED __attribute__((ns_returns_retained))
+#else
+#define NS_RETURNS_RETAINED
+#endif
+#endif
+
 #import "MNUnarchiver.h"
 #import "MNCIntermediateObjectProtocol.h"
 
